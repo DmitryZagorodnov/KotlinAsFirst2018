eae0ad93cc85c717fff144a96c04c77fca089834
DmitryZagorodnov <zagorodnov.dimka@gmail.com>
2018-12-08 19:56:27 +0500

    смь

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -189,11 +189,10 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *        )
  */
 fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<String>> {
-    var s: Set<String>
     val map = friends.toMutableMap()
     for ((name, set) in map) {
         for (element in set) {
-            if (element in map)  {s = (friends[element]!!.union(set)); map[name] = s - name}
+            if (element in map) map[name] = friends[element]!!.union(set) - name
             else map[element] = mutableSetOf()
         }
     }
